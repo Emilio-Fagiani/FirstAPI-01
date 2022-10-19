3256ff0a3e90327bcf9d59f348a92a1afc8e8bc6
Emilio <emi00@gmail.com>
2022-10-19 18:44:27 +0200

    feat: Created a class NameController

---
+++ src/main/java/it/develhope/FirstAPI01/NameController.java
@@ -0,0 +1,10 @@
+package it.develhope.FirstAPI01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/")
+public class NameController {
+
+}
