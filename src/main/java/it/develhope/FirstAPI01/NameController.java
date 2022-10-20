859f3a158890eaf36f5d177bf09c49a179201fe0
Emilio <emi00@gmail.com>
2022-10-20 11:32:01 +0200

    feat: "Add a get request"

--- src/main/java/it/develhope/FirstAPI01/NameController.java
+++ src/main/java/it/develhope/FirstAPI01/NameController.java
@@ -1,10 +1,19 @@
 package it.develhope.FirstAPI01;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/")
+@RequestMapping ( value = "/name")
 public class NameController {
 
+    @GetMapping
+    public String getName(){
+        return "Emilio";
+    }
+
+
 }
+
