e89b926cfecefa6ee9a9016a340d313006a5d4e1
Emilio <emi00@gmail.com>
2022-10-20 11:34:45 +0200

    feat: "Add a post request"

--- src/main/java/it/develhope/FirstAPI01/NameController.java
+++ src/main/java/it/develhope/FirstAPI01/NameController.java
@@ -1,10 +1,27 @@
 package it.develhope.FirstAPI01;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/")
+@RequestMapping ( value = "/name")
 public class NameController {
 
+    @GetMapping
+    public String getName(){
+        return "Emilio";
+    }
+
+    @PostMapping
+    public StringBuilder postRequest(){
+        StringBuilder sb = new StringBuilder(getName());
+        StringBuilder strReverse = sb.reverse();
+        return strReverse;
+    }
 }
+
+
+
+
